29586a240aa712320c2445ca3ccb074285f3e264
Khadim <khadimfall0602@gmail.com>
2024-04-22 01:41:55 +0200

    :)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const withNextra = require("nextra")({
+  output: "export",
+  basePath: "/pawnlink.docs",
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
+  images: {
+    unoptimized: true,
+  },
 });
 
 module.exports = withNextra();
